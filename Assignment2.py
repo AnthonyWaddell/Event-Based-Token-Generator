cd408721879f01ec78e269261d3b3368f45f4ddb
Anthony <23566571+AnthonyWaddell@users.noreply.github.com>
2018-11-01 23:25:48 -0700

    Added server button/textfield, and output label

--- Assignment2.py
+++ Assignment2.py
@@ -5,7 +5,6 @@ from PyQt4.QtCore import pyqtSlot
 from PyQt4.QtGui import *
 
 # Generate the random number
-#TODO: Do this three times
 randnum = os.urandom(128)
 hash = str(int(hashlib.sha256(randnum).hexdigest(),16))
 code = hash[:6]
@@ -14,24 +13,39 @@ code = hash[:6]
 app = QApplication(sys.argv)
 window = QWidget()
 window.setWindowTitle('Assignment 2')
-window.resize(600, 150)
+window.resize(900, 250)
  
-# Give window a text box
-textbox = QLineEdit(window)
-textbox.move(180, 80)
-textbox.resize(240,40)
+# Give window a text box for push button
+textbox_client = QLineEdit(window)
+textbox_client.move(180, 80)
+textbox_client.resize(240,40)
+
+# Give window a text box for token input
+textbox_server = QLineEdit(window)
+textbox_server.move(480, 80)
+textbox_server.resize(240,40)
+
  
-# Create a button in the window
-button = QPushButton('Generate Token', window)
-button.move(180,20)
+# Create a button to generate token
+create_button = QPushButton('Generate Token', window)
+create_button.move(180,20)
+
+# Create a button to input token
+input_button = QPushButton('Input Token', window)
+input_button.move(480,20)
  
-# Handle event
+# Handle event to generate token
 @pyqtSlot()
 def on_click():
-    textbox.setText(code)
+    textbox_client.setText(code)
+
+# Create a label to dsplay results to user
+window.labl=QLabel(window)
+window.labl.move(300,140)
+window.labl.setText("Testing") # change this later to reflect
  
-# Define click behavior
-button.clicked.connect(on_click)
+# Define token generation button behavior
+create_button.clicked.connect(on_click)
  
 # Dispaly to user
 window.show()
