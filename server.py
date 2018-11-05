895c1d8add16d6fc137fe7b0f79703caca035f16
Anthony <23566571+AnthonyWaddell@users.noreply.github.com>
2018-11-05 05:22:12 -0800

    Added comments && code to plot results

--- server.py
+++ server.py
@@ -16,7 +16,8 @@ def OTP(key):
     token = str(int(hashlib.sha256((key).encode("utf-8")).hexdigest(),16))[:6]
     Hash = str(int(hashlib.sha256((key).encode("utf-8")).hexdigest(),16))
     return Hash, token
-
+    
+#This function finds the index of a user inputted token value
 def yeet(value, search_list):
 	try:
 		list_index = search_list.index(value)
@@ -34,21 +35,17 @@ clicks = 0
 soon = False
 
 # Calcalulate 1,000,000 token generations
-for i in range(1000):
+for i in range(1000000):
 	hash_list.append(OTP(hash_list[i])[0])
 	token_list.append(OTP(hash_list[i])[1])
-	if i % 1000 == 0:
-		count.append(len(np.unique(token_list)))
+	if i % 10000 == 0:
+		count.append(len(token_list) - len(np.unique(token_list)))
 
 # Make a window
-
-for i in range(20):
-	print(token_list[i])
-
 app = QApplication(sys.argv)
 window = QWidget()
 window.setWindowTitle('Assignment 2 Server')
-window.resize(600, 250)
+window.resize(700, 250)
  
 # Give window a text box
 textbox = QLineEdit(window)
@@ -58,43 +55,48 @@ textbox.resize(240,40)
 # Create a button in the window
 button = QPushButton('Enter Password', window)
 button.move(180,20)
- 
 
 # Create a label to dsplay results to user
 window.labl=QLabel(window)
 window.labl.move(180,140)
-window.labl.resize(300,60)
+window.labl.resize(400,60)
 
 # Handle event
 @pyqtSlot()
 def on_click():
 	global clicks
 	global soon
+	# Get user inputted  toke
 	user_input = textbox.text()
-	print(user_input)
+	if user_input == None:
+		print(clicks)
+		user_input = ""
+	# Is this the correct token
 	if token_list[clicks] == user_input:
 		window.labl.setText("Access Granted")
 		clicks += 1
-		print(clicks)
+	# Is it an incorect token?
 	elif token_list[clicks] != user_input:
+		# Is at least gonna be correct soon?
 		for i in range(clicks, clicks + 10):
 			if token_list[i] == user_input:
 				window.labl.setText("Please try again")
-				print("before: ")
-				print(clicks)
 				clicks += 1
-				print("after: ")
-				print(clicks)
 				soon = True
 				break
+		# Or do we need to synchronize with the client?
 		if soon != True:
 			window.labl.setText("Please generate new token")
 			index = yeet(user_input, token_list)
 			clicks = index
 			clicks += 1
-			print("yeet: ")
-			print(clicks)
-				
+
+# Plot the number of collisions
+plt.plot(count)
+plt.ylabel("Number of Collisions")
+plt.xlabel(" Iterations in 10,000's")
+plt.title("Collisions Properties Evolutions as the number of OTP's Increase")
+plt.savefig("Graph", dpi = 400)
 
 # Define click behavior
 button.clicked.connect(on_click)
